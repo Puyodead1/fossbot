c3f7f7e3733eed2bde4311eb275d462d9160af5c
Puyodead1 <puyodead@proton.me>
2023-12-12 18:53:54 -0500

    Create MemberAdd.ts

---
+++ src/events/MemberAdd.ts
@@ -0,0 +1,19 @@
+import { Events, GuildMember } from "discord.js";
+import BaseClient from "../lib/BaseClient";
+import BaseEvent from "../lib/BaseEvent";
+
+export default class extends BaseEvent {
+    constructor(public readonly client: BaseClient) {
+        super({
+            event: Events.GuildMemberAdd,
+            once: false,
+        });
+    }
+
+    public async execute(member: GuildMember): Promise<void> {
+        // TODO: get welcome channel from database
+        const channel = await member.guild.channels.fetch("1006649184062836783");
+        if (!channel || !channel.isTextBased()) return;
+        await channel.send(`\`${member.user.tag}\` (\`${member.user.id}\`) has joined the party.`);
+    }
+}
