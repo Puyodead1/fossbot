9fe41f320621f051a1a1ce663f7eb7fb230f2bec
Puyodead1 <puyodead@proton.me>
2023-12-15 19:26:38 -0500

    fix channel lock/unlock commands

--- src/commands/LockChannel.ts
+++ src/commands/LockChannel.ts
@@ -17,22 +17,26 @@ export default class extends BaseCommand {
         const channel = await msg.guild?.channels.fetch(channelId);
         if (!channel || !("permissionOverwrites" in channel)) return await msg.channel.send("Invalid channel.");
 
-        const perms = channel.permissionOverwrites.cache.get(msg.guild!.id);
-        if (!perms) return await msg.channel.send("Couldn't get permission overwrites for channel.");
-        if (perms.deny.has("SendMessages")) {
-            const embed = new EmbedBuilder()
-                .setDescription(`🤔 Channel is already locked`)
-                .setColor("Red")
-                .setTimestamp()
-                .setFooter({
-                    text: msg.author.tag,
-                    iconURL: msg.author.displayAvatarURL(),
+        const perms = channel.permissionOverwrites.resolve(msg.guild!.id);
+        if (!perms) {
+            // if no permission overwrite for the @everyone role, create one
+            await channel.permissionOverwrites.create(msg.guild!.roles.everyone, { SendMessages: false });
+        } else {
+            if (perms.deny.has("SendMessages")) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`🤔 Channel is already locked`)
+                    .setColor("Red")
+                    .setTimestamp()
+                    .setFooter({
+                        text: msg.author.tag,
+                        iconURL: msg.author.displayAvatarURL(),
+                    });
+                return await msg.channel.send({
+                    embeds: [embed],
                 });
-            return await msg.channel.send({
-                embeds: [embed],
-            });
+            }
+            await channel.permissionOverwrites.edit(msg.guild!.roles.everyone, { SendMessages: false });
         }
-        await channel.permissionOverwrites.edit(msg.guild!.roles.everyone, { SendMessages: false });
 
         const embed = new EmbedBuilder()
             .setDescription(`🔒 Locked channel <#${channel.id}>`)
--- src/commands/UnlockChannel.ts
+++ src/commands/UnlockChannel.ts
@@ -18,21 +18,25 @@ export default class extends BaseCommand {
         if (!channel || !("permissionOverwrites" in channel)) return await msg.channel.send("Invalid channel.");
 
         const perms = channel.permissionOverwrites.cache.get(msg.guild!.id);
-        if (!perms) return await msg.channel.send("Couldn't get permission overwrites for channel.");
-        if (perms.allow.has("SendMessages")) {
-            const embed = new EmbedBuilder()
-                .setDescription(`🤔 Channel is not locked`)
-                .setColor("Red")
-                .setTimestamp()
-                .setFooter({
-                    text: msg.author.tag,
-                    iconURL: msg.author.displayAvatarURL(),
+        if (!perms) {
+            // if no permission overwrite for the @everyone role, create one
+            await channel.permissionOverwrites.create(msg.guild!.roles.everyone, { SendMessages: true });
+        } else {
+            if (perms.allow.has("SendMessages")) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`🤔 Channel is not locked`)
+                    .setColor("Red")
+                    .setTimestamp()
+                    .setFooter({
+                        text: msg.author.tag,
+                        iconURL: msg.author.displayAvatarURL(),
+                    });
+                return await msg.channel.send({
+                    embeds: [embed],
                 });
-            return await msg.channel.send({
-                embeds: [embed],
-            });
+            }
+            await channel.permissionOverwrites.edit(msg.guild!.roles.everyone, { SendMessages: true });
         }
-        await channel.permissionOverwrites.edit(msg.guild!.roles.everyone, { SendMessages: true });
 
         const embed = new EmbedBuilder()
             .setDescription(`🔓 Unlocked channel <#${channel.id}>`)
